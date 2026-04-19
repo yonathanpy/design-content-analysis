b330765f7f5719b2747864026f776287afcf29c3
Yonathan      aka (witwizard,orlserg) <109464957+yonathanpy@users.noreply.github.com>
2026-04-19 17:19:05 +0000

    Update db_loader.py

--- db_loader.py
+++ db_loader.py
@@ -1,1 +1,5 @@
-# db loader placeholder
+def extract_tables(cursor):
+    cursor.execute(
+        "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME LIKE 'field_data%';"
+    )
+    return [x[2] for x in cursor]
